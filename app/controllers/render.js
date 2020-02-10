1ae117dd4a097d24d0ad8cef981896b004c015cb
Eduardo Silvestre <eduardospg2903@gmail.com>
2020-02-10 13:51:08 -0300

    att de variaveis do som e endereço de midia

--- app/controllers/render.js
+++ app/controllers/render.js
@@ -1,7 +1,7 @@
 var importExp = require("../controllers/exp");
 var importAtrasoB = importExp.atrasoB;
 var importConfig = require("../model/configModel");
-var som = importConfig.vars.somDb;
+
 
 module.exports.intro = function(app, req, res){
 	var lastConfigModel = require("../model/configModel");
@@ -18,10 +18,12 @@ module.exports.index = function(app, req, res){
 }
 
 module.exports.exp = function(app, req, res){
+	var som = importConfig.vars.somDb;
 	res.render('exp',{atrasoB : importAtrasoB , som: som});
 }
 
 module.exports.expForc = function(app, req, res){
+	var som = importConfig.vars.somDb;
 	res.render('expForc',{atrasoB: importAtrasoB , som : som});
 	console.log("SOM: "+som);
 }
