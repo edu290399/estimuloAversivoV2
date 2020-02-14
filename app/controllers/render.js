18cad6b64fca39550aa3f312f9e86511a9de2fe5
Eduardo Silvestre <eduardospg2903@gmail.com>
2020-02-14 20:11:57 -0300

    tela de descanso DEPLOY

--- app/controllers/render.js
+++ app/controllers/render.js
@@ -1,10 +1,10 @@
 var importExp = require("../controllers/exp");
-var importAtrasoB = importExp.atrasoB;
+var importAtrasoB;
+var som;
 var importConfig = require("../model/configModel");
-
+var lastConfigModel = require("../model/configModel");
 
 module.exports.intro = function(app, req, res){
-	var lastConfigModel = require("../model/configModel");
 	res.render('intro',{txt02:lastConfigModel.vars.txtDb02 });
 }
 
@@ -13,24 +13,21 @@ module.exports.fim = function(app, req, res){
 }
 
 module.exports.index = function(app, req, res){
-	var lastConfigModel = require("../model/configModel");
 	res.render('index',{txt01:lastConfigModel.vars.txtDb01 });
 }
 
 module.exports.exp = function(app, req, res){
-	var som = importConfig.vars.somDb;
+	som = importConfig.vars.somDb;
+	importAtrasoB = importExp.atrasoB;
 	res.render('exp',{atrasoB : importAtrasoB , som: som});
 }
 
 module.exports.expForc = function(app, req, res){
-	var som = importConfig.vars.somDb;
+	som = importConfig.vars.somDb;
+	importAtrasoB = importExp.atrasoB;
 	res.render('expForc',{atrasoB: importAtrasoB , som : som});
 	console.log("SOM: "+som);
-}
-
-module.exports.sairDescanso= function(app, req, res){
-	console.log("Exp Forc apos descanso, com atrasoB = "+importAtrasoB);
-	res.render('expForc',{atrasoB: importAtrasoB});
+	console.log("Atraso de B DESCANSO: ",importAtrasoB);
 }
 
 module.exports.configRender = function(app, req, res){
